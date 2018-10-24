4792c8eeb400e5a41d573bc931dacd6418ad8573
Zi Yan <zi.yan@cs.rutgers.edu>
2018-10-24 10:50:46 -0400

    Test of parallel data access during vmalloc page migration.
    Signed-off-by: Zi Yan <zi.yan@cs.rutgers.edu>

--- vmalloc_test.c
+++ vmalloc_test.c
@@ -4,14 +4,35 @@
 #include <linux/vmalloc.h>
 #include <linux/kernel.h>
 #include <linux/migrate.h>
+#include <linux/kthread.h>
 
+MODULE_LICENSE("GPL");
+
+struct thread_data {
+	volatile int *data;
+};
+
+static int keep_reading(void *data)
+{
+	struct thread_data *arg = (struct thread_data *)data;
+	int tmp;
+
+	while (true) {
+		tmp += *(arg->data);
+		cond_resched();
+	};
+
+	return 0;
+}
 
 static int __init bench_init(void)
 {
 	char * src_addr = vmalloc(PAGE_SIZE);
-	int *data = (int *)src_addr;
+	volatile int *data = (int *)src_addr;
 	struct page *src_page, *dst_page;
 	struct page *after_src_page;
+	struct thread_data t_data;
+	struct task_struct *my_kthread;
 
 	memset(src_addr, 0, PAGE_SIZE);
 
@@ -23,12 +44,21 @@ static int __init bench_init(void)
 	pr_info("before: vaddr: %lx, src: %lx, dst: %lx\n", ((unsigned long)src_addr)>>PAGE_SHIFT, page_to_pfn(src_page), page_to_pfn(dst_page));
 
 	*data = 0x4f;
+	t_data.data = data;
+
+	my_kthread = kthread_run(keep_reading, &t_data, "test_vmalloc_migration");
+	pr_info("kthread launched\n");
 	
 	migrate_vmalloc_pages(src_addr, dst_page);
+	pr_info("migrate done\n");
 
 	after_src_page = vmalloc_to_page(src_addr);
+	pr_info("vmalloc_to_page done\n");
 
 	pr_info("after: vaddr: %lx, src: %lx, value: %x\n", ((unsigned long)src_addr)>>PAGE_SHIFT, page_to_pfn(after_src_page), *data);
+
+	kthread_stop(my_kthread);
+	pr_info("kthread stopped");
 
 	vfree(src_addr);
 	pr_info("after free src_addr: page ref: src: %d, dst: %d\n", page_count(src_page), page_count(dst_page));
