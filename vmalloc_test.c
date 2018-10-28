929b81901ff14145e2b50a5ca0ea488622401542
Zi Yan <zi.yan@cs.rutgers.edu>
2018-10-28 11:31:07 -0400

    more general migrate page test.
    Signed-off-by: Zi Yan <zi.yan@cs.rutgers.edu>

--- vmalloc_test.c
+++ vmalloc_test.c
@@ -31,7 +31,7 @@ static int __init bench_init(void)
 {
 	char * src_addr = vmalloc(PAGE_SIZE);
 	volatile int *data = (int *)src_addr;
-	struct page *src_page, *dst_page;
+	struct page *src_page;
 	struct page *after_src_page;
 	struct thread_data t_data;
 	struct task_struct *my_kthread;
@@ -39,11 +39,8 @@ static int __init bench_init(void)
 	memset(src_addr, 0, PAGE_SIZE);
 
 	src_page = vmalloc_to_page(src_addr);
-	dst_page = alloc_page(GFP_KERNEL);
 
-	pr_info("page ref: src: %d, dst: %d\n", page_count(src_page), page_count(dst_page));
-
-	pr_info("before: vaddr: %lx, src: %lx, dst: %lx\n", ((unsigned long)src_addr)>>PAGE_SHIFT, page_to_pfn(src_page), page_to_pfn(dst_page));
+	pr_info("before: vaddr: %lx, src: %lx\n", ((unsigned long)src_addr)>>PAGE_SHIFT, page_to_pfn(src_page));
 
 	*data = 0x4f;
 	t_data.data = data;
@@ -51,7 +48,7 @@ static int __init bench_init(void)
 	my_kthread = kthread_run(keep_reading, &t_data, "test_vmalloc_migration");
 	pr_info("kthread launched\n");
 	
-	migrate_vmalloc_pages(src_addr, dst_page);
+	migrate_vmalloc_pages(src_addr);
 	pr_info("migrate done\n");
 
 	after_src_page = vmalloc_to_page(src_addr);
@@ -63,9 +60,7 @@ static int __init bench_init(void)
 	pr_info("kthread stopped");
 
 	vfree(src_addr);
-	pr_info("after free src_addr: page ref: src: %d, dst: %d\n", page_count(src_page), page_count(dst_page));
-	__free_page(src_page);
-	pr_info("after free both: page ref: src: %d, dst: %d\n", page_count(src_page), page_count(dst_page));
+	pr_info("after free src_addr: page ref: src: %d, dst: %d\n", page_count(src_page), page_count(after_src_page));
 	return 0;
 }
 
